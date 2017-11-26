b737d11b89bd5282af1106ff4dab7c6cf6bf366f
sergabrod <sgrodriguez@gmail.com>
2017-11-26 10:13:52 -0300

    Autenticación de Usuarios

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,9 +8,6 @@ class TestController extends Controller
 {
     public function welcome()
     {
-        $a = 5;
-        $b = 10;
-        $c = $a + $b;
-        return 'El valor de la suma es ' . $c;
+        return view('welcome');
     }
 }
